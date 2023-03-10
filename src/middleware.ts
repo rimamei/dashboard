b7bf0bdd182838b7176e77b55b7f239a2f588768
Rima <rimameih@gmail.com>
2023-03-10 21:07:21 +0700

    feat: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+// middldware.ts
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+  const protectedPaths = ['/'];
+  const isPathProtected = protectedPaths?.some((path) => pathname == path);
+  const res = NextResponse.next();
+  if (isPathProtected) {
+    const url = new URL(`/dashboard`, req.url);
+    return NextResponse.redirect(url);
+  }
+  return res;
+}
