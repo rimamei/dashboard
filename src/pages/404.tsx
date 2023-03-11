8aab5594ae0e34261d0272853edc99e7cdc4e29a
Rima <rimameih@gmail.com>
2023-03-11 13:36:01 +0700

    feat: add page 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Custom404 = () => {
+  return (
+    <div className="mt-[100px] flex flex-col items-center justify-center">
+      <h1>404</h1>
+      <h4>Oops, Page not found</h4>
+    </div>
+  );
+};
+
+export default Custom404;
