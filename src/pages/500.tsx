463111b478a6db9ae56c997ce1ae9bbd4f7a8b0a
Rima <rimameih@gmail.com>
2023-03-12 09:35:39 +0700

    feat: add 500 page

---
+++ src/pages/500.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Custom500 = () => {
+  return (
+    <div className="min-h-[100vh] flex items-center justify-center">
+      <h1>Error 500</h1>
+    </div>
+  );
+};
+
+export default Custom500;
