77643a0f47def607f50f29eafc50978e976778c5
Rima <rimameih@gmail.com>
2023-03-26 22:58:34 +0700

    build: fix deploy

--- src/utils/Axios.ts
+++ src/utils/Axios.ts
@@ -3,9 +3,6 @@ import axios from 'axios';
 const Axios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API,
   timeout: 50000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 export default Axios;
